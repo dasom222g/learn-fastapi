cdd95666da502297962e0f3747015f8521afb861
dasom222g <dasom228@gmail.com>
2025-05-12 15:45:49 +0900

    feat: connect DB mysql

--- main.py
+++ main.py
@@ -1,63 +1,58 @@
 from typing import Annotated
+
+from sqlalchemy import create_engine
 import uvicorn
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+
 # FastAPI클래스로 웹 객체(인스턴스) 만듬
 templates = Jinja2Templates(directory="templates")
-app = FastAPI() # 클랙스
-app.mount('static', StaticFiles(directory='static'), name='static')
+app = FastAPI() # 클래스
 
 
-# 웹 객체에 path넣어주면 아래 함수 실행됨
-@app.get('/blog')
-def hello():
-  return {"message": "Hello world"}
+# MySQL 데이터베이스 연결 객체 생성
+db_connection = create_engine('mysql://test:1234@127.0.0.1:3306/test') # test 사용자, 비밀번호 1234로 localhost의 test 데이터베이스에 연결
+query = db_connection.execute('select * from player') # SQL 쿼리 실행: execute메서드는 SQL문 실행후 ResultProxy 객체 반환
+result = query.fetchall() # 모든 결과 행을 한 번에 가져와 튜플 리스트로 반환
+
+# for data in result:
+#   print('data', data)
+
+@app.get('/mysqltest')
+def mysqltest(request: Request):
+  query = db_connection.execute('select * from player')
+  result_db = query.fetchall()
+
+  result = []
+  for data in result_db:
+    item = {'player_id': data[0], 'player_name': data[1]}
+    result.append(item)
+
+  return templates.TemplateResponse('sql-test.html', {'request': request, 'result_list': result})
+
+# 쿼리 방식
+@app.get('/detail')
+def test_post(request: Request, id: str, name: str):
+    print(id, name)
+    # SQL Injection 방지를 위해 바인딩 처리
+    # select * from player where player_name like '_준'; 
+    query = db_connection.execute("SELECT * FROM player WHERE player_id={} and player_name like '%{}%'".format(id, name))
+    result_db = query.fetchall()
+    result = []
+    for i in result_db:
+        temp = {
+            'player_id': i[0],
+            'player_name': i[1],
+            'team_name': i[2],
+            'height': i[-2],
+            'weight': i[-1]
+        }
+        result.append(temp)
+    return templates.TemplateResponse("detail.html", {"request": request, "result_list": result})
 
 
-@app.get('/test')
-def test(request: Request):
-  print('request----------')
-  # print(request)  # request 전체 객체 출력
-  # print(request.body())  # 요청 body 출력 (POST/PUT 등)
-  # print(request.method)  # GET, POST 등
-  # print(request.url)     # 요청 URL
-  # print(request.headers) # 요청 헤더
-  # print(request.cookies) # 요청 쿠키
-  print('request----------')
-
-  return templates.TemplateResponse('test.html', {'request': request, 'a': 2})
-
-# 경로 파라미터
-@app.get('/test/{name}')
-def arg_print(request: Request, name: str, age: int):
-  # name은 문자열로 형변환, age는 숫자로 형변환하여 받음
-  print(f'name: {name}')
-  return templates.TemplateResponse('test.html', {'request': request, 'a': 2})
-
-# 쿼리 파라미터
-@app.get('/test2')
-def arg_print(request: Request, name: str, age: str):
-  print(f'name: {name} age: {age}')
-  return templates.TemplateResponse('test.html', {'request': request, 'a': 2})
-
-# test2.html 전송
-@app.get('/front_get')
-def send_frontend(request: Request, name: str, age: str):
-  return templates.TemplateResponse('test2.html', {'request': request, 'name': name, 'age': age})
-
-# post_test.html 전송
-@app.get('/post_page')
-def post_page(request: Request):
-  return templates.TemplateResponse('post_test.html', {'request': request})
-
-# form타입의 사용자 입력값 가져오기
-@app.post('/test_post')
-def test_post(name: Annotated[str, Form()], pwd: Annotated[int, Form()]):
-  print(name, pwd)
-
-# python3 main.py만 실행해도 [uvicorn main:app --reload]로 서버 띄우게끔 세팅
 if __name__ == '__main__':
   print('서버on')
   uvicorn.run(app, host='localhost', port=8080)
